8919a8c38293dc437a1d74073f1b9b2ac70df3c3
sleroy <sylvain.leroy@tocea.com>
2017-09-04 16:12:18 +0200

    Added abstractLayout export

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ module.exports = {
 	bootstrap3: require("./layouts/bootstrap3.vue"),
 	component: require("./formGenerator.vue"),
 	schema: require("./utils/schema.js"),
+	abstractLayout: require("./layouts/abstractLayout"),
 	validators: require("./utils/validators.js"),
 	abstractField: require("./fields/abstractField").default,
 
